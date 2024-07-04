c5a6eeaff69343f842b0a439ab5350c79d38ef8f
Igor <megakill123@yandex.ru>
2024-07-04 15:48:52 +0500

    edit project

--- main/assets/gameobjects/items/chest.go
+++ main/assets/gameobjects/items/chest.go
@@ -13,3 +13,48 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "coins_dialog"
+  component: "/main/assets/gui/chest_dialogs/coins_dialog.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "death"
+  component: "/main/assets/scripts/death.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "potion_dialog"
+  component: "/main/assets/gui/chest_dialogs/potion_dialog.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
