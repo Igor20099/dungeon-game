3947d268b2dcdd65d7d153f01196d8728156625e
Igor <megakill123@yandex.ru>
2024-07-15 00:22:39 +0500

    edit porject

--- main/assets/gameobjects/items/chest.go
+++ main/assets/gameobjects/items/chest.go
@@ -58,3 +58,138 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "armor"
+  component: "/main/assets/gui/chest_dialogs/armor.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "blood_sword"
+  component: "/main/assets/gui/chest_dialogs/blood_sword.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "golden_sword"
+  component: "/main/assets/gui/chest_dialogs/golden_sword.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "health_sword"
+  component: "/main/assets/gui/chest_dialogs/health_sword.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "light_armor"
+  component: "/main/assets/gui/chest_dialogs/light_armor.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "poison_sword"
+  component: "/main/assets/gui/chest_dialogs/poison_sword.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "rusty_sword"
+  component: "/main/assets/gui/chest_dialogs/rusty_sword.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "stamina_sword"
+  component: "/main/assets/gui/chest_dialogs/stamina_sword.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "sword_straight"
+  component: "/main/assets/gui/chest_dialogs/sword_straight.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
