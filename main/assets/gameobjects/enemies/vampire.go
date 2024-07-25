585aaaf72a03e5bd4b20c0618900a41e43961bf5
Igor241241 <megakill123@yandex.ru>
2024-07-25 09:41:11 +0500

    edit project

--- main/assets/gameobjects/enemies/vampire.go
+++ main/assets/gameobjects/enemies/vampire.go
@@ -1,47 +1,14 @@
 components {
   id: "enemy"
   component: "/main/assets/gui/enemies/vampire.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "death"
   component: "/main/assets/scripts/components/death.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "enemy_stats"
   component: "/main/assets/scripts/enemy/enemy_stats.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
   properties {
     id: "type"
     value: "vampire"
@@ -79,32 +46,7 @@ embedded_components {
   data: "size {\n"
   "  x: 128.0\n"
   "  y: 32.0\n"
-  "  z: 0.0\n"
-  "  w: 0.0\n"
   "}\n"
-  "color {\n"
-  "  x: 1.0\n"
-  "  y: 1.0\n"
-  "  z: 1.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "outline {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "shadow {\n"
-  "  x: 0.0\n"
-  "  y: 0.0\n"
-  "  z: 0.0\n"
-  "  w: 1.0\n"
-  "}\n"
-  "leading: 1.0\n"
-  "tracking: 0.0\n"
-  "pivot: PIVOT_CENTER\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  "line_break: false\n"
   "text: \"\\320\\222\\320\\260\\320\\274\\320\\277\\320\\270\\321\\200\"\n"
   "font: \"/builtins/fonts/default.font\"\n"
   "material: \"/builtins/fonts/label-df.material\"\n"
@@ -112,12 +54,9 @@ embedded_components {
   position {
     x: 614.0
     y: 128.0
-    z: 0.0
   }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
+  scale {
+    x: 1.0E-6
+    y: 1.0E-6
   }
 }
